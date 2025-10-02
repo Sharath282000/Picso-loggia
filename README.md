e9aeff4fe0d99aaa1aa0396af2c7a9dc7ea4f423
Sharath282000 <sharathmurugan28@gmail.com>
2025-10-02 19:19:52 +0530

    Adding files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pics-Loggia
+Pics Loggia - A gallery of world's best photographs developed in React Js and Pexels api.
+
+## Output
+![alt text](image.png)
+
+## Link
+https://pics-loggia.netlify.app/
---
+++ src/request.jsx
@@ -0,0 +1,13 @@
+import {createClient} from 'pexels';
+
+const key= import.meta.env.VITE_API_KEY;
+
+const api_key = createClient(key);
+
+const requests ={
+    fetchRandom : api_key.photos.curated({ per_page: 100 }),
+    searchPhoto:api_key.photos,
+    getPhoto:api_key.photos,
+}
+
+export default requests;
